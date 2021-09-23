6b01b994801a432b2c1bd72c6cda608c011a9b91
Xun Ruan <galaxymacos@gmailc.om>
2021-09-23 22:53:29 +0800

    Initial Commit

---
+++ FirstApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  FirstApp
+//
+//  Created by Xun Ruan on 2021/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ByeView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Xun Ruan on 2021/9/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        // MARK: In macOS
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
